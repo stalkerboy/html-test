a2231e9785b8d951a496c5790f3cee6a44342fad
unknown <hughjunglee@gmail.com>
2019-09-30 17:41:39 +0900

    fabricjs example add

---
+++ js/fabricjs/sprite.class.js
@@ -0,0 +1,86 @@
+fabric.Sprite = fabric.util.createClass(fabric.Image, {
+  type: "sprite",
+
+  spriteWidth: 32,
+  spriteHeight: 64,
+  spriteIndex: 0,
+  frameTime: 100,
+  direction: 1,
+
+  initialize: function(element, options) {
+    options || (options = {});
+
+    options.width = this.spriteWidth;
+    options.height = this.spriteHeight;
+
+    this.callSuper("initialize", element, options);
+
+    this.createTmpCanvas();
+    this.createSpriteImages();
+  },
+
+  createTmpCanvas: function() {
+    this.tmpCanvasEl = fabric.util.createCanvasElement();
+    this.tmpCanvasEl.width = this.spriteWidth || this.width;
+    this.tmpCanvasEl.height = this.spriteHeight || this.height;
+  },
+
+  createSpriteImages: function() {
+    this.spriteImages = [];
+    direction = this.direction;
+
+    var steps = this._element.width / this.spriteWidth;
+    for (var i = 0; i < steps; i++) {
+      this.createSpriteImage(i, direction);
+    }
+  },
+
+  createSpriteImage: function(i, direction) {
+    var tmpCtx = this.tmpCanvasEl.getContext("2d");
+    tmpCtx.clearRect(0, 0, this.tmpCanvasEl.width, this.tmpCanvasEl.height);
+    tmpCtx.drawImage(
+      this._element,
+      -i * this.spriteWidth,
+      -direction * this.spriteHeight
+    );
+
+    var dataURL = this.tmpCanvasEl.toDataURL("image/png");
+    var tmpImg = fabric.util.createImage();
+
+    tmpImg.src = dataURL;
+
+    this.spriteImages.push(tmpImg);
+  },
+
+  _render: function(ctx) {
+    ctx.drawImage(
+      this.spriteImages[this.spriteIndex],
+      -this.width / 2,
+      -this.height / 2
+    );
+  },
+
+  play: function() {
+    var _this = this;
+    this.animInterval = setInterval(function() {
+      _this.onPlay && _this.onPlay();
+      _this.dirty = true;
+      _this.spriteIndex++;
+      if (_this.spriteIndex === _this.spriteImages.length) {
+        _this.spriteIndex = 0;
+      }
+    }, this.frameTime);
+  },
+
+  stop: function() {
+    clearInterval(this.animInterval);
+  }
+});
+
+fabric.Sprite.fromURL = function(url, callback, imgOptions) {
+  fabric.util.loadImage(url, function(img) {
+    callback(new fabric.Sprite(img, imgOptions));
+  });
+};
+
+fabric.Sprite.async = true;
